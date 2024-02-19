5e86f14a509575f844f2f7a1059d54ed1e3fa33c
lipi150 <112479235+lipi150@users.noreply.github.com>
2024-02-19 22:11:50 +0530

    edit

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -11,11 +11,19 @@ const Leaderboard = () => {
         <>
             <div className='bg-bgc1 lg:bg-bgc5 rounded-lg lg:box-shadow'>
                 <div className='flex flex-row'>
-                    <div className='lg:box-shadow bg-bgc1 h-10 flex items-center p-3 rounded-lg text-sm flex-grow'>Leaderboard</div>
-                    <div className='flex p-2 bg-bgc5 cursor-pointer font-bold hover:bg-bgc2 hover:text-black box-shadow rounded-lg lg:hidden ml-10' onClick={()=>setShow(!show)}><button>{show? 'hide' :'show'} Leaderboard</button></div>
+                    <div className=' bg-bgc1 h-10 flex items-center p-3 rounded-lg text-sm flex-grow lg:box-shadow'>Leaderboard</div>
+                    <div className='flex p-2 bg-bgc5 cursor-pointer font-bold hover:bg-bgc2 hover:text-black box-shadow rounded-lg lg:hidden ml-10' onClick={() => setShow(!show)}><button>{show ? 'hide' : 'show'} Leaderboard</button></div>
                 </div>
+
+                <div className='pt-2 pl-4 pr-4 pb-2 flex-col gap-2 overflow-y-scroll h-[18rem] hidden lg:flex'>
+                    {leaderData.map((item) => (
+                        <LeaderCard id={item.id} names={item.names} srno={item.srno} />
+                    ))}
+
+                </div>
+
                 {show &&
-                    <div className='pt-2 pl-4 pr-4 pb-2 flex flex-col gap-2 overflow-y-scroll h-[18rem]'>
+                    <div className='pt-2 pl-4 pr-4 pb-2 flex flex-col gap-2 overflow-y-scroll h-[18rem]  lg:hidden'>
                         {leaderData.map((item) => (
                             <LeaderCard id={item.id} names={item.names} srno={item.srno} />
                         ))}
